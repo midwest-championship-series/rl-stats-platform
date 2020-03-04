decd1807c9391ebc06c237b248f1a6c1145c7609
corbfon <corbin.fonville@gmail.com>
2020-03-04 13:27:25 -0600

    stop trying to write game output

--- src/process_game.py
+++ src/process_game.py
@@ -1,4 +1,4 @@
 import carball
 
 def analyzeGame():
-  return carball.decompile_replay('temp/example.replay', overwrite=True, output_path='temp/example.json')
+  return carball.decompile_replay('temp/example.replay')
