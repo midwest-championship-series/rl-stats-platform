c11e8462690688b7fee6eeb756304b382da99cbd
corbfon <corbin.fonville@gmail.com>
2020-03-31 06:09:43 -0500

    refactor report-games for better error handling

--- lambdas/report-games.js
+++ lambdas/report-games.js
@@ -1,13 +1,8 @@
 const reportGames = require('../src/report-games')
-const games = require('../src/model/games')
 
 const handler = async (event, context) => {
   try {
-    const body = JSON.parse(event.body)
-    if (body.match_id) {
-      body.game_ids = (await games.get({ criteria: { match_id: body.match_id } })).map(game => game.game_id)
-    }
-    const reportedGames = await reportGames(body)
+    const reportedGames = await reportGames(JSON.parse(event.body))
     return {
       statusCode: 200,
       body: JSON.stringify(reportedGames),
--- src/report-games.js
+++ src/report-games.js
@@ -4,7 +4,11 @@ const teamGames = require('./model/team-games')
 const playerGames = require('./model/player-games')
 const processMatch = require('./producers')
 
-module.exports = async ({ game_ids }) => {
+module.exports = async ({ match_id, game_ids }) => {
+  if (match_id) {
+    game_ids = (await games.get({ criteria: { match_id } })).map(game => game.game_id)
+    if (game_ids.length === 0) throw new Error(`no game_ids found for match_id: ${match_id}`)
+  }
   const reportGames = await ballchasing.getReplays({ game_ids })
   const { gameStats, teamStats, playerStats } = await processMatch(reportGames)
   await games.upsert({ data: gameStats })
