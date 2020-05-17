25846435a49bd42b1d8630428db24aa46c421e9a
corbfon <corbin.fonville@gmail.com>
2020-05-17 12:53:31 -0500

    proxy v1 leagues

--- src/api/v1.js
+++ src/api/v1.js
@@ -4,16 +4,42 @@ const tables = {
   games: require('../model/sheets/games'),
   members: require('../model/sheets/members'),
   schedules: require('../model/sheets/schedules'),
+  // leagues: {
+  //   get: async () => {
+  //     const [leagueData, scheduleData] = await Promise.all([
+  //       require('../model/sheets/leagues').get({ json: true }),
+  //       require('../model/sheets/schedules').get({ json: true }),
+  //     ])
+  //     leagueData.forEach(league => {
+  //       league.schedule = scheduleData.filter(s => s.league_id === league.id)
+  //     })
+  //     return leagueData
+  //   },
+  // },
   leagues: {
     get: async () => {
-      const [leagueData, scheduleData] = await Promise.all([
-        require('../model/sheets/leagues').get({ json: true }),
-        require('../model/sheets/schedules').get({ json: true }),
+      const leagues = await require('../model/mongodb/leagues').Model.aggregate([
+        { $lookup: { from: 'seasons', as: 'seasons', localField: 'season_ids', foreignField: '_id' } },
+        { $lookup: { from: 'teams', as: 'teams', localField: 'team_ids', foreignField: '_id' } },
+        { $addFields: { matches: { $arrayElemAt: ['$seasons', 0] } } },
+        { $addFields: { matches: '$matches.match_ids' } },
+        { $lookup: { from: 'matches', as: 'schedule', localField: 'matches', foreignField: '_id' } },
       ])
-      leagueData.forEach(league => {
-        league.schedule = scheduleData.filter(s => s.league_id === league.id)
+      leagues.forEach(league => {
+        league.id = league._id
+        league.schedule.forEach(match => {
+          match.id = match._id
+          const teams = league.teams.filter(t => match.team_ids.map(t => t.toHexString()).includes(t._id.toHexString()))
+          match.team_1_id = teams[0]._id
+          match.team_1_name = teams[0].name
+          match.team_2_id = teams[1]._id
+          match.team_2_name = teams[1].name
+          match.season = '1'
+          match.type = 'REG'
+          match.league_id = league._id
+        })
       })
-      return leagueData
+      return leagues
     },
   },
   'player-games': require('../model/sheets/player-games'),
