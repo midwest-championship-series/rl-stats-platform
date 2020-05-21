16a6d366f98fc1c0202bbe8f9da8a0616ea7e78f
corbfon <corbin.fonville@gmail.com>
2020-05-21 00:42:16 -0500

    update reprocess-games to v2 data

--- src/__tests__/reprocess-games.js
+++ src/__tests__/reprocess-games.js
@@ -1,30 +1,59 @@
-const reprocessGames = require('../reprocess-games')
+const { ObjectId } = require('mongodb')
 
 // mocks
-const games = require('../model/sheets/games')
-jest.mock('../model/sheets/games')
-const schedules = require('../model/sheets/schedules')
-jest.mock('../model/sheets/schedules')
+jest.mock('../services/mongodb')
+const matches = require('../model/mongodb/matches')
+jest.mock('../model/mongodb/matches')
+const matchesFindMock = jest.fn()
+matches.Model = {
+  find: jest.fn(() => ({ populate: matchesFindMock })),
+}
 const aws = require('../services/aws')
 jest.mock('../services/aws')
 
+const reprocessGames = require('../reprocess-games')
+
 describe('reprocess-games', () => {
   process.env.GAMES_QUEUE_URL = 'fake queue url'
-  it('should reprocess games', async () => {
-    schedules.get.mockResolvedValue([{ id: '123' }, { id: '456' }, { id: '789' }])
-    games.get.mockResolvedValue([
-      { game_id: 'game 1 id', match_id: '123' },
-      { game_id: 'game 2 id', match_id: '123' },
-      { game_id: 'game 3 id', match_id: '123' },
-      { game_id: 'game 1 id', match_id: '789' },
+  it('should reprocess matches', async () => {
+    matchesFindMock.mockResolvedValueOnce([
+      {
+        _id: new ObjectId('5ebc62b0d09245d2a7c63401'),
+        games: [
+          { ballchasing_id: '595ac248-5f25-48a5-bf39-9b50f25e97a1' },
+          { ballchasing_id: 'b63a3a3b-6b3d-433a-ab21-8a6c02d6bd8e' },
+          { ballchasing_id: 'a1ed2167-3f3f-46e0-b198-ef765d4adac6' },
+          { ballchasing_id: '877f66a5-23c9-4397-9c47-97c9870351c0' },
+        ],
+      },
+      {
+        _id: new ObjectId('5ebc62b0d09245d2a7c63402'),
+        games: [
+          { ballchasing_id: '126f416e-d845-45b9-b843-8afe300d4e2a' },
+          { ballchasing_id: 'ab847408-5927-442b-b888-c5a2d68c96e4' },
+          { ballchasing_id: 'c813c19a-2a4e-4df9-9fda-608425b1a356' },
+        ],
+      },
     ])
     await reprocessGames({ season: 1 })
     expect(aws.sqs.sendMessageBatch).toHaveBeenCalledWith('fake queue url', [
       {
-        game_ids: ['game 1 id', 'game 2 id', 'game 3 id'],
-        match_id: '123',
+        match_id: '5ebc62b0d09245d2a7c63401',
+        game_ids: [
+          '595ac248-5f25-48a5-bf39-9b50f25e97a1',
+          'b63a3a3b-6b3d-433a-ab21-8a6c02d6bd8e',
+          'a1ed2167-3f3f-46e0-b198-ef765d4adac6',
+          '877f66a5-23c9-4397-9c47-97c9870351c0',
+        ],
       },
-      { game_ids: ['game 1 id'], match_id: '789' },
+      {
+        match_id: '5ebc62b0d09245d2a7c63402',
+        game_ids: [
+          '126f416e-d845-45b9-b843-8afe300d4e2a',
+          'ab847408-5927-442b-b888-c5a2d68c96e4',
+          'c813c19a-2a4e-4df9-9fda-608425b1a356',
+        ],
+      },
     ])
   })
 })
--- src/reprocess-games.js
+++ src/reprocess-games.js
@@ -1,19 +1,14 @@
-const schedule = require('./model/sheets/schedules')
-const games = require('./model/sheets/games')
+const { Model: Matches } = require('./model/mongodb/matches')
 const sqs = require('./services/aws').sqs
 
 module.exports = async criteria => {
-  const matches = (await schedule.get({ criteria, json: true })).map(m => m.id)
-  const processedGames = (await games.get({ json: true })).filter(g => matches.includes(g.match_id))
-  const messages = processedGames.reduce((result, game) => {
-    const matchMessage = result.find(m => m.match_id === game.match_id)
-    if (matchMessage) {
-      matchMessage.game_ids.push(game.game_id)
-    } else {
-      result.push({ match_id: game.match_id, game_ids: [game.game_id] })
+  const matches = await Matches.find(criteria).populate('games')
+  const messages = matches.map(match => {
+    return {
+      match_id: match._id.toHexString(),
+      game_ids: match.games.map(g => g.ballchasing_id),
     }
-    return result
-  }, [])
+  })
   await sqs.sendMessageBatch(process.env.GAMES_QUEUE_URL, messages)
   return { messages }
 }
