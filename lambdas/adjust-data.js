69e01ae5603c7621b718e4f934e27c1a14265783
corbfon <corbin.fonville@gmail.com>
2020-05-21 20:31:06 -0500

    only change current_week

--- lambdas/adjust-data.js
+++ lambdas/adjust-data.js
@@ -8,11 +8,10 @@ const handler = async () => {
     populate: { path: 'matches' },
   })
   for (let league of leagues) {
-    const [currentSeason] = league.seasons.slice(-1)
+    const currentSeason = league.seasons.find(s => s._id.equals(league.current_season_id))
     if (!currentSeason) throw new Error(`no current season found for league: ${league.name}`)
     const sortedMatches = currentSeason.matches.sort((a, b) => a.week - b.week)
     league.current_week = sortedMatches.find(m => m.status !== 'closed').week
-    league.current_season_id = currentSeason._id
     await league.save()
   }
   console.log('finished adjusting')
