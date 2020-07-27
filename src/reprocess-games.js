4dc373d9281cf968ecc7ec56927e8b6855246ab8
corbfon <corbin.fonville@gmail.com>
2020-07-27 15:03:56 -0500

    update reprocess games to query leagues and seasons

--- src/reprocess-games.js
+++ src/reprocess-games.js
@@ -1,14 +1,49 @@
 const { Model: Matches } = require('./model/mongodb/matches')
+const { Model: Seasons } = require('./model/mongodb/seasons')
+const { Model: Leagues } = require('./model/mongodb/leagues')
 const sqs = require('./services/aws').sqs
 
-module.exports = async criteria => {
-  const matches = await Matches.find({ 'game_ids.0': { $exists: true }, ...criteria }).populate('games')
-  const messages = matches.map(match => {
-    return {
+// Array.prototype.reduceSubArray =
+
+// const modelMap = {
+//   matches: Matches,
+//   seasons: Seasons,
+// }
+
+const reduceArray = (docs, prop) => {
+  return docs.reduce((result, item) => {
+    result.push(...item[prop])
+    return result
+  }, [])
+}
+
+const matchGetters = {
+  matches: criteria => {
+    return Matches.find({ 'game_ids.0': { $exists: true }, ...criteria }).populate('games')
+  },
+  seasons: criteria => {
+    return Seasons.find({ ...criteria })
+      .populate('matches')
+      .populate('games')
+      .then(seasons => reduceArray(seasons, 'matches'))
+  },
+  leagues: criteria => {
+    return Leagues.find({ ...criteria })
+      .populate('seasons')
+      .populate('matches')
+      .populate('games')
+      .then(leagues => reduceArray(leagues, 'seasons'))
+      .then(seasons => reduceArray(seasons, 'matches'))
+  },
+}
+
+module.exports = async (collection, criteria) => {
+  const messages = (await matchGetters[collection](criteria))
+    .filter(match => match.games && match.games.length > 0)
+    .map(match => ({
       match_id: match._id.toHexString(),
       game_ids: match.games.map(g => g.ballchasing_id),
-    }
-  })
+    }))
   await sqs.sendMessageBatch(process.env.GAMES_QUEUE_URL, messages)
   return { messages }
 }
