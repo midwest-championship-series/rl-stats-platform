b5631c78f6a02c6b33b35f24c85a177e33e5a822
Corbin Fonville <corbin.fonville@gmail.com>
2020-08-08 16:34:17 -0500

    add error handler that posts to mnrl bot (#24)

--- src/services/get-page.js
+++ src/services/get-page.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default
 
-export default async (platform, platformId) => {
+module.exports = async (platform, platformId) => {
   const webpage = await axios.get(`https://rocketleague.tracker.network/profile/${platform}/${platformId}`)
   return cheerio.load(webpage.data)
 }
