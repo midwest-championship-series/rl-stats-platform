fa4239daa1148197a12b88646dd0290392c89711
corbfon <corbin.fonville@gmail.com>
2020-08-08 16:41:38 -0500

    add avatar to players

--- src/model/mongodb/teams.js
+++ src/model/mongodb/teams.js
@@ -7,12 +7,18 @@ const schema = {
   hex_color: { type: String },
   /** @deprecated all the below properties are deprecated 8/3/2020 */
   league_id: { type: Schema.Types.ObjectId },
-  league: { type: String }, // should go away in favor of league_id
   old_id: { type: String },
   old_league_id: { type: String },
 }
 
-const Model = createModel('Team', schema)
+const Model = createModel('Team', schema, schema => {
+  schema.set('toJSON', {
+    ...schema.toJSON,
+    transform: function(doc, ret) {
+      ret.league = 'mncs'
+    },
+  })
+})
 
 module.exports = {
   get: ({ criteria }) => Model.find(criteria).exec(),
