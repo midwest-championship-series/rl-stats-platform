86fcbe04d9dca2f3265f4cfc9107ec2673351295
corbfon <corbin.fonville@gmail.com>
2020-08-08 18:14:28 -0500

    flexible event processing

--- lambdas/process-match.js
+++ lambdas/process-match.js
@@ -2,7 +2,7 @@ const processMatch = require('../src/process-match')
 require('../src/model/mongodb')
 
 const handler = async event => {
-  const messages = event.Records.map(r => JSON.parse(r.body))
+  const messages = event.Records.map(r => (typeof r.body === 'string' ? JSON.parse(r.body) : r.body))
   for (let message of messages) {
     const { match_id, game_ids } = await processMatch(message)
     console.log(`processed match: ${match_id}, games: ${game_ids.join(', ')}`)
