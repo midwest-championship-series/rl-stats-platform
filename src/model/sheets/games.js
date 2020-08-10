42ff10fdda58da059873d183401a069194db36b2
corbfon <corbin.fonville@gmail.com>
2020-08-10 18:07:13 -0500

    remove legacy sheets links

--- src/model/sheets/games.js
+++
@@ -1,4 +0,0 @@
-const Table = require('../../services/google')
-const { MNCS } = require('./constants')()
-
-module.exports = new Table('games', MNCS.spreadsheetId, MNCS.games, ['game_id'])
--- src/model/sheets/leagues.js
+++
@@ -1,7 +0,0 @@
-const Table = require('../../services/google')
-const { MNCS } = require('./constants')()
-
-const leagues = new Table('games', MNCS.spreadsheetId, MNCS.leagues)
-leagues.addPopulation({ table: 'schedules', localField: 'id', foreignField: 'league_id', as: 'schedule' })
-
-module.exports = leagues
