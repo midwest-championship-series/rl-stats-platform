f700a66532bbb432fa48fbabf3fad7f2bdd46278
Corbin Fonville <corbin.fonville@gmail.com>
2020-08-16 16:01:27 -0500

    add avatar property to teams (#36)

--- src/model/mongodb/teams.js
+++ src/model/mongodb/teams.js
@@ -5,6 +5,7 @@ const schema = {
   name: { type: String },
   discord_id: { type: String },
   hex_color: { type: String },
+  avatar: { type: String },
 }
 
 const Model = createModel('Team', schema)
