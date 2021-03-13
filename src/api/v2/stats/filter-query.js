80a675eebc3d86ba9d67b3ca4b277ebd9f71a038
Corbin Fonville <corbin.fonville@gmail.com>
2021-03-13 16:26:01 -0600

    add a filter query endpoint (#54)

---
+++ src/api/v2/stats/filter-query.js
@@ -0,0 +1,20 @@
+const stage = process.env.SERVERLESS_STAGE
+
+const { search } = require('../../../services/elastic')
+
+module.exports = indexName => {
+  return async (req, res, next) => {
+    const params = req.query
+    const query = {
+      query: {
+        bool: {
+          filter: Object.entries(params).map(([key, value]) => {
+            return { term: { [key]: value } }
+          }),
+        },
+      },
+    }
+    req.context = await search(`${stage}_stats_${indexName}`, query)
+    next()
+  }
+}
