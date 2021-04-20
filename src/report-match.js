353b390785ea12a4ca7adb1cc64404a4c884e0b0
corbfon <corbin.fonville@gmail.com>
2021-04-20 00:31:58 -0500

    add reporting replays as part of the normal workflow

--- src/report-match.js
+++ src/report-match.js
@@ -8,15 +8,11 @@ const replayBucket = process.env.REPLAY_FILES_BUCKET
 module.exports = async (params) => {
   const { league_id, urls, reply_to_channel } = params
   const game_ids = urls.map((url) => url.split('?')[0].split('/').slice(-1)[0])
+  const gameIdsToProcess = [...new Set(game_ids)]
   /**
    * if games with these ballchasing ids have already been reported, throw an error - we only want new games to
    * be reported with this mechanism. all other games should go through the reprocess-games function.
    */
-  const games = await Games.find({ ballchasing_id: { $in: game_ids } })
-  if (games.length > 0) {
-    throw new Error('games have already been reported - please use the !reprocess command')
-  }
-  const gameIdsToProcess = [...new Set(game_ids)]
   const replays = []
   for (let id of gameIdsToProcess) {
     const uploadSource = 'ballchasing'
@@ -33,14 +29,17 @@ module.exports = async (params) => {
       },
     })
   }
-  const detail = { game_ids: gameIdsToProcess, league_id, reply_to_channel }
-  await eventBridge.emitEvent({
-    type: 'MATCH_PROCESS_INIT',
-    detail,
-  })
   await eventBridge.emitEvent({
     type: 'MATCH_PROCESS_REPLAYS_OBTAINED',
     detail: { replays },
+  })
+  const games = await Games.find({ ballchasing_id: { $in: gameIdsToProcess } })
+  if (games.length > 0) {
+    throw new Error('games have already been reported - please use the !reprocess command')
+  }
+  await eventBridge.emitEvent({
+    type: 'MATCH_PROCESS_INIT',
+    detail: { game_ids: gameIdsToProcess, league_id, reply_to_channel },
   })
 
   return { replays }
