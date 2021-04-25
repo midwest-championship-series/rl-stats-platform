034347c6a036218f18e8cc4c669aaac889112f0e
Benjamin Nylund <31191383+bnylund@users.noreply.github.com>
2021-04-25 11:59:14 -0500

    added colors variable to teams model, awaiting transition

--- src/model/mongodb/teams.js
+++ src/model/mongodb/teams.js
@@ -5,6 +5,12 @@ const schema = {
   name: { type: String },
   discord_id: { type: String },
   hex_color: { type: String },
+  colors: {
+    home_primary: { type: String },
+    home_secondary: { type: String },
+    away_primary: { type: String },
+    away_secondary: { type: String },
+  },
   avatar: { type: String },
   franchise_id: { type: Schema.Types.ObjectId },
 }
