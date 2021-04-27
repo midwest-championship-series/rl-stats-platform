395ccbef70dcd4344befb555a3f8b37aafeaa65e
corbfon <corbin.fonville@gmail.com>
2021-04-26 21:35:24 -0500

    unset ballchasing_id

--- lambdas/migrate.js
+++ lambdas/migrate.js
@@ -2,17 +2,15 @@ require('../src/model/mongodb')
 const { Games } = require('../src/model/mongodb')
 
 const handler = async () => {
-  const games = await Games.find({ ballchasing_id: { $exists: true } })
-  games.forEach((game) => {
-    game.replay_origin = {
-      source: 'ballchasing',
-      key: game.ballchasing_id,
-    }
-  })
-  for (let game of games) {
-    await game.save()
-  }
-  console.log(`saved ${games.length} games`)
+  const { nModified } = await Games.updateMany(
+    {},
+    {
+      $unset: {
+        ballchasing_id: '',
+      },
+    },
+  )
+  console.log(`updated ${nModified} games`)
 }
 
 module.exports = { handler }
