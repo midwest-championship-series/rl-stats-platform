660dadd45d76d30f434b1c2f7134b755b122eff7
Corbin Fonville <corbin.fonville@gmail.com>
2021-08-20 12:36:11 -0500

    delete nsi stuff (#84)

--- lambdas/migrate.js
+++ lambdas/migrate.js
@@ -3,11 +3,29 @@ const path = require('path')
 const { Matches, Leagues, Seasons } = require('../src/model/mongodb')
 
 const handler = async () => {
-  const leagues = await Leagues.find()
-  for (let league of leagues) {
-    league.default_timezone = 'America/Chicago'
-    await league.save()
+  const [league] = await Leagues.find({ name: 'nsi' }).populate({
+    path: 'seasons',
+    populate: {
+      path: 'matches',
+      populate: {
+        path: 'games',
+      },
+    },
+  })
+  for (let season of league.seasons) {
+    for (let match of season.matches) {
+      for (let game of match.games) {
+        console.log('deleting game', game._id)
+        await game.remove()
+      }
+      console.log('deleting match', match._id)
+      await match.remove()
+    }
+    console.log('deleting season', season._id)
+    await season.remove()
   }
+  console.log('deleting league', league._id)
+  await league.remove()
 }
 
 module.exports = { handler }
