6c49ddb29ad097261768db60af99fb9de821b94c
corbfon <corbin.fonville@gmail.com>
2022-02-13 13:12:42 -0600

    migrations to delete current seasons

--- lambdas/migrate.js
+++ lambdas/migrate.js
@@ -1,66 +1,28 @@
 const fs = require('fs')
 const path = require('path')
+const compareIds = require('../src/util/compare-ids')
 const { Matches, Leagues, Seasons } = require('../src/model/mongodb')
-
-const parseSchedule = (fileName) => {
-  return JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'temp', fileName)))
-}
-
-const formatMatches = (schedule) => {
-  return schedule.map((match) => {
-    return new Matches({
-      team_ids: [match.team_1_id, match.team_2_id],
-      players_to_teams: [],
-      match_type: 'REG',
-      week: match.week,
-      best_of: 5,
-      scheduled_datetime: new Date(`${match.scheduled_date} ${match.scheduled_time}`),
-    })
-  })
-}
-
-const createSchedule = async (leagueName, seasonName, schedule) => {
-  console.log(`formatting ${leagueName} matches`)
-  const matches = formatMatches(schedule)
-  const league = await Leagues.findOne({ name: leagueName }).populate('seasons')
-  const newSeason = league.seasons.find((s) => s.name === seasonName)
-  if (newSeason.match_ids.length < 1) {
-    for (let match of matches) {
-      console.log('saving match', match._id)
-      await match.save()
-      newSeason.match_ids.push(match._id)
-    }
-    await newSeason.save()
-    console.log(`${league.name} season ${seasonName} updated`)
-  }
-}
-
 const handler = async () => {
-  const newSeasonName = '5'
-  // make sure season exists
-  console.log('getting leagues')
-  const leagues = await Leagues.find({
-    $or: [{ name: 'mncs' }, { name: 'clmn' }, { name: 'mnrs' }],
-  }).populate('seasons')
+  const leagues = await Leagues.find({ $or: [{ name: 'mncs' }, { name: 'clmn' }, { name: 'mnrs' }] }).populate({
+    path: 'current_season',
+    populate: {
+      path: 'matches',
+    },
+  })
+
   for (let league of leagues) {
-    let newSeason = league.seasons.find((s) => s.name === newSeasonName)
-    if (!newSeason) {
-      console.log(`${league.name} has no season ${newSeasonName}`)
-      newSeason = new Seasons({
-        name: newSeasonName,
-        season_type: 'REG',
-        match_ids: [],
-      })
-      await newSeason.save()
-      league.season_ids.push(newSeason._id)
+    console.log('adjusting', league.name)
+    if (league.current_season) {
+      for (let match of league.current_season.matches) {
+        console.log('match!')
+        await match.remove()
+      }
+      await league.current_season.remove()
     }
-    league.current_season_id = newSeason._id
+    league.season_ids = league.season_ids.filter((s) => !compareIds(s._id, league.current_season_id))
+    console.log(league.current_season_id, league.seasons)
     await league.save()
   }
-
-  await createSchedule('mncs', newSeasonName, parseSchedule('mncs.json'))
-  await createSchedule('clmn', newSeasonName, parseSchedule('clmn.json'))
-  await createSchedule('mnrs', newSeasonName, parseSchedule('mnrs.json'))
 }
 
 module.exports = { handler }
