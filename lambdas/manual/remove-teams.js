3880563f0c30ba5e5a59055ab18854f27757d1d3
corbfon <corbin.fonville@gmail.com>
2022-07-07 17:06:24 -0500

    add manual lambdas to adjust some data

---
+++ lambdas/manual/remove-teams.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const { Players } = require('../../src/model/mongodb')
+
+const handler = async () => {
+  const players = await Players.find({
+    team_history: { $elemMatch: { date_joined: { $exists: true }, date_left: { $exists: false } } },
+  })
+  for (let player of players) {
+    player.team_history.forEach((history) => {
+      if (!history.date_left) {
+        history.date_left = new Date()
+      }
+    })
+    console.log('saving', player.screen_name)
+    await player.save()
+  }
+}
+
+module.exports = { handler }
