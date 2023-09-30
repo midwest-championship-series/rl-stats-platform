8a46a26fb3fdaa1256582f929e586ff4dfefe330
Corbin Fonville <corbin.fonville@gmail.com>
2023-09-30 16:28:39 -0500

    season 8 player unlink migration

--- lambdas/migrate.js
+++ lambdas/migrate.js
@@ -6,48 +6,22 @@ const handler = async () => {
   const players = await Players.find({
     team_history: {
       $elemMatch: {
-        date_left: { $gt: new Date('2023-03-01T00:00:00.535+00:00') },
-        date_joined: { $lt: new Date('2023-02-28T00:00:00.535+00:00') },
+        date_left: { $exists: false },
       },
     },
   })
+
+  console.log('modifying players:', players.length)
   for (let player of players) {
-    // console.log(player)
-    // const dateItem = player.team_history.find(item => {
-    //   return !item.date_left
-    // })
-
-    const dateItem = player.team_history.find((item) => {
-      return (
-        item.date_left > new Date('2023-03-01T00:00:00.535+00:00') &&
-        item.date_joined < new Date('2023-02-28T00:00:00.535+00:00')
-      )
+    player.team_history.forEach((item) => {
+      if (!item.date_left) {
+        item.date_left = new Date('2023-08-01T06:00:00.535+00:00')
+      }
     })
-    if (!dateItem.date_left) {
-      continue
-    }
 
-    dateItem.date_left = new Date('2023-03-01T00:00:00.535+00:00')
     await player.save()
-
-    console.log(player.screen_name, dateItem)
   }
-
-  // const joinDate = new Date('2023-03-01T00:00:00.535+00:00')
-  // const players = await Players.find({ team_history: { $elemMatch: {
-  //   date_left: {$exists: false},
-  //   date_joined: { $gt: joinDate }
-  // }}})
-  // for (let player of players) {
-  //   // console.log(player)
-  //   const dateItem = player.team_history.find(item => {
-  //     return item.date_joined > joinDate && !item.date_left
-  //   })
-  //   dateItem.date_joined = joinDate
-  //   await player.save()
-  //   console.log(player.screen_name, dateItem)
-
-  // }
+  console.log('DONE!')
 }
 
 module.exports = { handler }
