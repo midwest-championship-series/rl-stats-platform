aea57192b754855b1a753b1e9ef0a165b5031c29
Corbin Fonville <corbin.fonville@gmail.com>
2023-10-28 13:12:32 -0500

    add franchises

--- src/schemas/contexts/team_context.js
+++ src/schemas/contexts/team_context.js
@@ -4,4 +4,6 @@ module.exports = [
   { name: 'team_id', type: types.STR },
   { name: 'team_name', type: types.STR },
   { name: 'team_color', type: types.STR },
+  { name: 'franchise_id', type: types.STR },
+  { name: 'franchise_name', type: types.STR },
 ]
